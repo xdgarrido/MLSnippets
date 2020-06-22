067bc34eeb3035532a2e695c1a4b13f6770c4b1b
xdgarrido <dgarrido@amd.com>
2020-06-22 10:45:40 -0700

    clean-up

--- ndocker_run.sh
+++
@@ -1,9 +0,0 @@
-PWD=`pwd`
-DOCKER_DIR=/`basename $PWD`
-CTNRNAME=CudaContainer
-# -u `id -u`:`id -g
-IMAGE=nvcr.io/nvidia/tensorflow:20.03-tf2-py3
-#IMAGE=tensorflow/tensorflow:2.2.0rc1-gpu-py3
-docker run -it --name $CTNRNAME --gpus 1 --network=host --shm-size=16g  --ulimit memlock=-1 --ulimit stack=67108864 -v=`pwd`:$DOCKER_DIR -v /data:/data -w $DOCKER_DIR --privileged --rm --device=/dev/kfd --device=/dev/dri --group-add video  --security-opt seccomp=unconfined $IMAGE 
-
-
